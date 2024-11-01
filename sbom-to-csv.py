af0f9794377a8c2d849ea77263cc0d1c59bdd3a2
Ирхин Артем (АСКОН) <irkhin_aa@ascon.ru>
2024-11-01 16:06:02 +0000

    implementation of sbom to csv script

---
+++ sbom-to-csv.py
@@ -0,0 +1,57 @@
+# SPDX-FileCopyrightText: 2024 Artem Irkhin
+# SPDX-License-Identifier: Apache-2.0
+
+import json, csv, argparse
+
+
+def get_prop(arr, name):
+    for elem in arr:
+        if elem.get('name', '') == name:
+            return elem.get('value', '')
+    return ''
+
+
+parser = argparse.ArgumentParser(description='генератор таблицы компонентов в формате csv')
+parser.add_argument('input', help='входной файл, содержащий перечень заимствованных компонентов, в JSON формате')
+parser.add_argument('output', help='выходной файл в формате csv, содержащий таблицу со всеми компонентами из входного файла')
+args = parser.parse_args()
+
+with open(args.input, 'r') as f:
+    bom_json = json.load(f)
+
+with open(args.output, 'w', newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(['№ п/п','Наименование компонента', 'Версия компонента', 'Язык (языки) программирования, на котором написан компонент', 'Принадлежность компонента к поверхности атаки программного обеспечения и (или) к компонентам, реализующим функции безопасности', 'Адрес веб-ресурса, на котором расположен исходный код компонента'])
+
+
+
+stack = bom_json.get('components', []).copy()
+idx = 1
+
+while stack:
+    component = stack.pop(0)
+    if 'components' in component:
+        stack += component['components']
+
+    ext_refs = component.get('externalReferences')
+    urls = ''
+    if ext_refs:
+        for item in ext_refs:
+            if item['type'] == 'vcs':
+                urls+=f'Репозиторий: {item["url"]}\n'
+            elif item['type'] == 'website':
+                urls+=f'Адрес веб-ресурса компонента: {item["url"]}\n'
+            else:
+                urls+=f'Иное: {item["url"]}\n'
+    special_function = {"GOST:attack_surface":"yes/indirect/no", "GOST:security_function":"yes/indirect/no"}
+    ext_refs = component.get('properties', [])
+    attack_surface = get_prop(ext_refs, 'GOST:attack_surface')
+    if attack_surface in ['yes', 'indirect', 'no']: special_function["GOST:attack_surface"] = attack_surface
+    security_function = get_prop(ext_refs, 'GOST:security_function')
+    if security_function in ['yes', 'indirect', 'no']: special_function["GOST:security_function"] = security_function
+    with open(args.output, 'a', newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([idx, component['name'], component['version'],'',special_function, urls])
+    idx += 1
+
+
