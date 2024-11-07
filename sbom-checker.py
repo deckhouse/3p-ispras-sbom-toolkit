dc17f84ee9cf9461f4944b8290b3a76c8851ba1c
Ekaterina Shastun <shastun@ispras.ru>
2024-11-07 18:30:59 +0300

    changed reference resolution for schema in sbom-checker.py to avoid "is not a 'uri-reference'" error on Windows

--- sbom-checker.py
+++ sbom-checker.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import jsonschema
 from pathlib import Path
+from referencing import Registry, Resource
 
 parser = argparse.ArgumentParser(description='проверка sbom-файлов')
 parser.add_argument('json', help='файл-спецификация; по умолчанию ./schema.json', nargs='?', default='./schema.json')
@@ -16,20 +17,28 @@ args = parser.parse_args()
 with open(args.json) as f:
     schema = json.load(f)
 
+registry = None
 if Path(args.json).samefile(Path('./schema.json')): # to resolve references to schemas on local filesystem
-    schema["$defs"]["license"]["properties"]["id"]["$ref"] = \
-        "file://" + str(Path(__file__).parent.resolve() / 'additional_schemas' / \
-        schema["$defs"]["license"]["properties"]["id"]["$ref"])
-    schema["$defs"]["signature"]["$ref"] = \
-        "file://" + str(Path(__file__).parent.resolve() / 'additional_schemas' / \
-        schema["$defs"]["signature"]["$ref"])
+    with open(Path(__file__).parent.resolve() / 'additional_schemas' / "spdx.schema.json") as f:
+        resource1 = Resource.from_contents(json.load(f))
+    with open(Path(__file__).parent.resolve() / 'additional_schemas' / "jsf-0.82.schema.json") as f:
+        resource2 = Resource.from_contents(json.load(f))
+    registry = Registry().with_resources(
+        [
+            ("spdx.schema.json", resource1),
+            ("jsf-0.82.schema.json", resource2),
+        ],
+    )
 
 with open(args.filename) as f:
     try:
         parsed_file = json.load(f)
         cls = jsonschema.validators.validator_for(schema)
         cls.check_schema(schema)
-        validator = cls(schema, format_checker=cls.FORMAT_CHECKER)
+        if registry:
+            validator = cls(schema, format_checker=cls.FORMAT_CHECKER, registry=registry)
+        else:
+            validator = cls(schema, format_checker=cls.FORMAT_CHECKER)
         errors = validator.iter_errors(parsed_file)
         count = 0
         limit = args.errors
