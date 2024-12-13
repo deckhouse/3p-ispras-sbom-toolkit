8a182355d873b9c27e4dcca94345a45c47658ed5
Ekaterina Shastun <shastun@ispras.ru>
2024-12-13 15:03:36 +0300

    added --check-vcs option to sbom-checker.py

--- sbom-checker.py
+++ sbom-checker.py
@@ -12,6 +12,7 @@ parser.add_argument('json', help='файл-спецификация; по умо
 parser.add_argument('filename', help='входной файл в формате CycloneDX JSON для проверки')
 parser.add_argument('-e', '--errors', type=int, default=10,
                     help='максимальное число ошибок для вывода; по умолчанию 10; установите 0 для вывода всех ошибок')
+parser.add_argument('--check-vcs', action='store_true', help='проверка url типа vcs на git-репозиторий')
 
 args = parser.parse_args()
 with open(args.json) as f:
@@ -48,7 +49,24 @@ with open(args.filename) as f:
             print('-'*50)
             if limit and count == limit:
                 break
-        if count == 0:
+        if args.check_vcs:
+            from git.cmd import Git
+            _git = Git()
+            stack = parsed_file.get('components', []).copy()
+            not_repos = 0
+            while stack:
+                component = stack.pop(0)
+                for url in component.get('externalReferences', []):
+                    if url.get('type', '') == 'vcs':
+                        try:
+                            ls_res = _git.ls_remote(url.get('url', ''))
+                        except Exception:
+                            not_repos += 1
+                            print(f"{url['url']} не является git-репозиторием")
+                            print('-'*50)
+            if not_repos == 0 and count == 0:
+                print('файл корректный')
+        elif count == 0:
             print('файл корректный')
     except jsonschema.exceptions.SchemaError as se:
         print('ошибка в файле-спецификации:')
