e701f1ad7e26d306f2d5baabc3663311dc5d7990
Ekaterina Shastun <shastun@ispras.ru>
2024-12-23 15:16:17 +0300

    added properties field to top-level output components in sbom-unifier.py script

--- sbom-unifier.py
+++ sbom-unifier.py
@@ -5,6 +5,24 @@ import argparse
 import datetime
 import json
 
+def get_prop(arr, name):
+    for elem in arr:
+        if elem.get('name', '') == name:
+            return elem.get('value', '')
+    return ''
+
+def eval_prop(components, name):
+    vals = set()
+    for comp in components:
+        vals.add(get_prop(comp.get('properties', []), name))
+    if 'yes' in vals:
+        return 'yes'
+    if 'indirect' in vals:
+        return 'indirect'
+    if vals == {'no'}:
+        return 'no'
+    return ''
+
 parser = argparse.ArgumentParser(description='объединение sbom-файлов')
 parser.add_argument('--app-name', required=True, help='название продукта')
 parser.add_argument('--app-version', required=True, help='версия продукта')
@@ -27,14 +45,22 @@ all_components = []
 for fn in args.input:
     with open(fn, 'r', encoding='utf-8') as f:
         data = json.load(f)
-        new_data = {
-            'type': data['metadata']['component']['type'],
-            'name': data['metadata']['component']['name'],
-            'version': data['metadata']['component']['version'],
-        }
+        new_data = data['metadata']['component'].copy()
         for key in keys:
             if key in data:
                 new_data[key] = data[key]
+        if not 'properties' in new_data:
+            new_data['properties'] = []
+        if not get_prop(new_data.get('properties', []), 'GOST:attack_surface'):
+            new_data['properties'].append({
+                "name": 'GOST:attack_surface',
+                "value": eval_prop(data.get('components', []), 'GOST:attack_surface')
+            })
+        if not get_prop(new_data.get('properties', []), 'GOST:security_function'):
+            new_data['properties'].append({
+                "name": 'GOST:security_function',
+                "value": eval_prop(data.get('components', []), 'GOST:security_function')
+            })
         all_components.append(new_data)
 
 output_data = {
